d223f38209b1148dbb0f57b16e9309614aea804a
deShortOne <jingshianggu@gmail.com>
2024-08-27 08:35:48 +0100

    feat: Create category if not already exists

---
+++ MoneyTracker/Database/Category.cs
@@ -0,0 +1,31 @@
+﻿using Npgsql;
+
+namespace MoneyTracker.API.Database
+{
+    public class Category
+    {
+        public async Task<int> AddCategory(string categoryName)
+        {
+            // UPSERTS!! and gets id
+            var queryGetIdOfCategoryName = """
+                INSERT INTO category (name) VALUES
+                    (@categoryName)
+                ON CONFLICT (name) DO NOTHING
+                RETURNING id;
+                """;
+            var queryGetIdOfCategoryNameParams = new List<NpgsqlParameter>()
+            {
+                new NpgsqlParameter("categoryName", categoryName),
+            };
+
+            // get category id
+            using var readerId = await Helper.GetTable(queryGetIdOfCategoryName, queryGetIdOfCategoryNameParams);
+            await readerId.ReadAsync();
+            if (readerId == null || !readerId.HasRows)
+            {
+                throw new InvalidOperationException("Category name already exists");
+            }
+            return readerId.GetInt32(0);
+        }
+    }
+}
