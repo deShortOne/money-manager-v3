fe4ef507c4832b215fed96c7950b85a9bddb5e1f
deShortOne <jingshianggu@gmail.com>
2024-08-27 08:40:08 +0100

    feat: add category controller

---
+++ MoneyTracker/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoneyTracker.API.Database;
+using MoneyTracker.API.Models;
+using MoneyTracker.Controllers;
+using System.Net;
+
+namespace MoneyTracker.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public CategoryController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpPost(Name = "AddCategory")]
+        public async Task<int> AddBudget(string categoryName)
+        {
+            return await new Category().AddCategory(categoryName);
+        }
+    }
+}
