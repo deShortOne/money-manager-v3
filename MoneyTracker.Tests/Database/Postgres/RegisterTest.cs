954c51a9a569c85a4e882b92b878db4c28d93308
deShortOne <jingshianggu@gmail.com>
2024-09-01 17:05:41 +0100

    test: temp not doing

--- MoneyTracker.Tests/Database/Postgres/RegisterTest.cs
+++ MoneyTracker.Tests/Database/Postgres/RegisterTest.cs
@@ -3,12 +3,14 @@ using MoneyTracker.Shared.Models.Transaction;
 using Newtonsoft.Json;
 using Testcontainers.PostgreSql;
 
-namespace MoneyTracker.Tests
+namespace MoneyTracker.Tests.Database.Postgres
 {
     public sealed class RegisterTest : IAsyncLifetime
     {
         private readonly PostgreSqlContainer _postgres = new PostgreSqlBuilder()
+#if RUN_LOCAL
             .WithDockerEndpoint("tcp://localhost:2375")
+#endif
             .WithImage("postgres:16")
             .WithCleanUp(true)
             .Build();
@@ -33,6 +35,95 @@ namespace MoneyTracker.Tests
 
             var registerRes = new List<TransactionDTO>()
             {
+                new TransactionDTO() {
+                    Id = 1,
+                    Payee = "Company A",
+                    Amount = 1800,
+                    DatePaid = DateTime.Parse("2024-08-28T00:00:00Z").ToUniversalTime(),
+                    Category = "Wages & Salary : Net Pay",
+                },
+                new TransactionDTO() {
+                    Id = 6,
+                    Payee = "Supermarket",
+                    Amount = 27,
+                    DatePaid = DateTime.Parse("2024-08-15T00:00:00Z").ToUniversalTime(),
+                    Category = "Groceries",
+                },
+                new TransactionDTO() {
+                    Id = 7,
+                    Payee = "Hobby item",
+                    Amount = 150,
+                    DatePaid = DateTime.Parse("2024-08-09T00:00:00Z").ToUniversalTime(),
+                    Category = "Hobby",
+                },
+                new TransactionDTO() {
+                    Id = 5,
+                    Payee = "Supermarket",
+                    Amount = 23,
+                    DatePaid = DateTime.Parse("2024-08-08T00:00:00Z").ToUniversalTime(),
+                    Category = "Groceries",
+                },
+                new TransactionDTO() {
+                    Id = 4,
+                    Payee = "Supermarket",
+                    Amount = 25,
+                    DatePaid = DateTime.Parse("2024-08-01T00:00:00Z").ToUniversalTime(),
+                    Category = "Groceries",
+                },
+                new TransactionDTO() {
+                    Id = 2,
+                    Payee = "Phone company",
+                    Amount = 10,
+                    DatePaid = DateTime.Parse("2024-08-01T00:00:00Z").ToUniversalTime(),
+                    Category = "Bills : Cell Phone",
+                },
+                new TransactionDTO() {
+                    Id = 3,
+                    Payee = "Landlord A",
+                    Amount = 500,
+                    DatePaid = DateTime.Parse("2024-08-01T00:00:00Z").ToUniversalTime(),
+                    Category = "Bills : Rent",
+                }
+            };
+            var lis = await register.GetAllTransactions();
+            if (registerRes.Count != lis.Count)
+            {
+                Assert.Fail("Length not equal!");
+            }
+            for (int i = 0; i < lis.Count; i++)
+            {
+                if (JsonConvert.SerializeObject(registerRes[i]) != JsonConvert.SerializeObject(lis[i]))
+                {
+                    Assert.Fail($"Item #{i} failed\nExpected:\n{JsonConvert.SerializeObject(registerRes[i])}\n" +
+                        $"but got:\n{JsonConvert.SerializeObject(lis[i])}");
+                }
+            }
+        }
+
+        //[Fact]
+        public async void AddItemAfterFirstLoad()
+        {
+            var db = new Helper(_postgres.GetConnectionString());
+            var register = new Register(db);
+            var transactionToAdd = new TransactionDTO()
+            {
+                Id = 8,
+                Payee = "Super star",
+                Amount = 2300,
+                DatePaid = DateTime.Parse("2024-09-01T00:00:00Z").ToUniversalTime(),
+                Category = "Hobby",
+            };
+            await register.AddNewTransaction(new NewTransactionDTO()
+            {
+                Amount = transactionToAdd.Amount,
+                Category = -1, // oh no
+                DatePaid = transactionToAdd.DatePaid,
+                Payee = transactionToAdd.Payee,
+            });
+
+            var registerRes = new List<TransactionDTO>()
+            {
+                transactionToAdd,
                 new TransactionDTO() {
                     Id = 1,
                     Payee = "Company A",
