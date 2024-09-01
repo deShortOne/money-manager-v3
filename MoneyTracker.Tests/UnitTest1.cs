7fbb41744414e3cc5c50a3aac1a9a26279c8dc9c
deShortOne <jingshianggu@gmail.com>
2024-09-01 13:11:09 +0100

    test: does not pass but does connect

--- MoneyTracker.Tests/UnitTest1.cs
+++ MoneyTracker.Tests/UnitTest1.cs
@@ -1,11 +1,32 @@
+using MoneyTracker.Data.Postgres;
+using MoneyTracker.Shared.Models.Transaction;
+using Testcontainers.PostgreSql;
+
 namespace MoneyTracker.Tests
 {
-    public class UnitTest1
+    public sealed class UnitTest1 : IAsyncLifetime
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly PostgreSqlContainer _postgres = new PostgreSqlBuilder()
+            .WithDockerEndpoint("tcp://localhost:2375")
+            .WithImage("postgres:16")
+            .WithCleanUp(true)
+            .Build();
 
+        public Task InitializeAsync()
+        {
+            return _postgres.StartAsync();
+        }
+
+        public Task DisposeAsync()
+        {
+            return _postgres.DisposeAsync().AsTask();
+        }
+
+        [Fact]
+        public async void Test1()
+        {
+            var register = new Register();
+            Assert.Equal(new List<TransactionDTO>(), await register.GetAllTransactions());
         }
     }
 }
