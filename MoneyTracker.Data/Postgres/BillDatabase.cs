e76040894bc83888b2188cd6f0b63e61bb23c31c
deShortOne <jingshianggu@gmail.com>
2024-09-03 21:58:21 +0100

    feat: added bill database

---
+++ MoneyTracker.Data/Postgres/BillDatabase.cs
@@ -0,0 +1,18 @@
+﻿
+using MoneyTracker.Data.Global;
+using MoneyTracker.Shared.Models.Bill;
+
+namespace MoneyTracker.Data.Postgres;
+public class BillDatabase : IBillDatabase
+{
+    private readonly PostgresDatabase _database;
+    public BillDatabase(IDatabase db)
+    {
+        _database = (PostgresDatabase)db;
+    }
+
+    public Task<List<BillDTO>> GetBill()
+    {
+        return null;
+    }
+}
