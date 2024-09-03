20c68e52f6a5854b17b1c2a81d1a4dcf925a123b
deShortOne <jingshianggu@gmail.com>
2024-09-03 22:16:01 +0100

    feat: added bill controller

---
+++ MoneyTracker.API/Controllers/BillController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoneyTracker.Data.Global;
+using MoneyTracker.Shared.Models.Bill;
+
+namespace MoneyTracker.API.Controllers;
+public class BillController : ControllerBase
+{
+
+    private readonly ILogger<BillController> _logger;
+    private readonly IBillDatabase _database;
+
+    public BillController(ILogger<BillController> logger, IBillDatabase db)
+    {
+        _logger = logger;
+        _database = db;
+    }
+
+    [HttpGet]
+    [Route("get")]
+    public Task<List<BillDTO>> Get()
+    {
+        return _database.GetBill();
+    }
+}
