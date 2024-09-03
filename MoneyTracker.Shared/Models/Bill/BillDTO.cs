514980708490bce7d1aebed8d3a6d71d7390b534
deShortOne <jingshianggu@gmail.com>
2024-09-03 21:55:14 +0100

    feat: added bill dto

---
+++ MoneyTracker.Shared/Models/Bill/BillDTO.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MoneyTracker.Shared.Models.Bill;
+public class BillDTO(int id, string payee, decimal amount, DateOnly nextDueDate, string frequency, string category)
+{
+    public int Id { get; private set; } = id;
+    public string Payee { get; private set; } = payee;
+    public decimal Amount { get; private set; } = amount;
+    public DateOnly NextDueDate { get; private set; } = nextDueDate;
+    public string Frequency { get; private set; } = frequency;
+    public string Category { get; private set; } = category;
+}
