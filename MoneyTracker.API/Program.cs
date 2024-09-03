b30fb42ec48a99944c7a990f61e6ed43b776b34d
deShortOne <jingshianggu@gmail.com>
2024-09-03 22:35:48 +0100

    feat: dateonly set to accept string
    instead of whatever weird object it was trying

--- MoneyTracker.API/Program.cs
+++ MoneyTracker.API/Program.cs
@@ -1,4 +1,5 @@
-﻿using MoneyTracker.Data.Global;
+﻿using Microsoft.OpenApi.Models;
+using MoneyTracker.Data.Global;
 using MoneyTracker.Data.Postgres;
 using MoneyTracker.DatabaseMigration;
 
@@ -9,7 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.MapType<DateOnly>(() => new OpenApiSchema
+    {
+        Type = "string",
+        Format = "date"
+    });
+});
 
 var dbConnString = builder.Configuration["Database:Paelagus_RO"];
 builder.Services.AddSingleton<IDatabase>(_ => new PostgresDatabase(dbConnString));
