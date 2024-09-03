300ef70541563e72459c2e4e671915fe04556dd4
deShortOne <jingshianggu@gmail.com>
2024-09-03 18:41:52 +0100

    feat: connection string set in program.cs

--- MoneyTracker.Data/Postgres/PostgresDatabase.cs
+++ MoneyTracker.Data/Postgres/PostgresDatabase.cs
@@ -7,26 +7,18 @@ namespace MoneyTracker.Data.Postgres
 {
     public class PostgresDatabase : IDatabase
     {
-        private readonly NpgsqlDataSource dataSource_rw;
+        private readonly NpgsqlDataSource _dataSource_rw;
 
         // TODO: have ro and rw strings
         public PostgresDatabase(string connectionString)
         {
-            if (connectionString == null || connectionString == string.Empty)
-            {
-                IConfigurationRoot config = new ConfigurationBuilder()
-                    .AddUserSecrets<SecretKey>()
-                    .Build();
-                connectionString = config["Database:Paelagus_RO"];
-            }
-
             NpgsqlDataSourceBuilder dataSourceBuilder_ro = new NpgsqlDataSourceBuilder(connectionString);
-            dataSource_rw = dataSourceBuilder_ro.Build();
+            _dataSource_rw = dataSourceBuilder_ro.Build();
         }
 
         public async Task<DbDataReader> GetTable(string query, List<DbParameter> parameters = null)
         {
-            var conn = await dataSource_rw.OpenConnectionAsync();
+            var conn = await _dataSource_rw.OpenConnectionAsync();
 
             await using (var cmd = new NpgsqlCommand(query, conn))
             {
@@ -43,7 +35,7 @@ namespace MoneyTracker.Data.Postgres
 
         public async Task<int> UpdateTable(string query, List<DbParameter> parameters = null)
         {
-            var conn = await dataSource_rw.OpenConnectionAsync();
+            var conn = await _dataSource_rw.OpenConnectionAsync();
 
             await using (var cmd = new NpgsqlCommand(query, conn))
             {
