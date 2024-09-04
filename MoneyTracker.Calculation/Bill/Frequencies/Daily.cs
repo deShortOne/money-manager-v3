a1acfce7753ea2afa0ce699f539a9939d82772c9
deShortOne <jingshianggu@gmail.com>
2024-09-04 08:25:01 +0100

    feat: implemented daily

---
+++ MoneyTracker.Calculation/Bill/Frequencies/Daily.cs
@@ -0,0 +1,19 @@
+﻿
+using MoneyTracker.Shared.Models.Bill;
+
+namespace MoneyTracker.Calculation.Bill.Frequencies;
+internal class Daily : IFrequency
+{
+    public OverDueBillInfo? Calculate(DateOnly nextDueDate)
+    {
+        var today = DateOnly.FromDateTime(DateTime.Now);
+        if (today == nextDueDate)
+        {
+            return null;
+        }
+
+        int numberOfDaysOverdue = nextDueDate.CompareTo(today);
+        return new OverDueBillInfo(numberOfDaysOverdue, numberOfDaysOverdue);
+    }
+    public bool MatchCommand(string frequency) => frequency.Equals("Daily");
+}
