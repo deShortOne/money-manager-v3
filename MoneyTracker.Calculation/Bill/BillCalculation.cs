9e56bf2155fdd94d0a9c67cf71545ef2019857f3
deShortOne <jingshianggu@gmail.com>
2024-09-04 08:20:56 +0100

    feat: added command pattern for frequencies

---
+++ MoneyTracker.Calculation/Bill/BillCalculation.cs
@@ -0,0 +1,20 @@
+﻿using MoneyTracker.Calculation.Bill.Frequencies;
+using MoneyTracker.Shared.Models.Bill;
+
+namespace MoneyTracker.Calculation.Bill;
+
+public class BillCalculation
+{
+    private static readonly List<IFrequency> Frequencies = [];
+    public static OverDueBillInfo? CalculateOverDueBillInfo(DateOnly nextDueDate, string frequency)
+    {
+        foreach (var f in Frequencies)
+        {
+            if (f.MatchCommand(frequency))
+            {
+                return f.Calculate(nextDueDate);
+            }
+        }
+        throw new NotImplementedException("Frequency type \"{frequency}\" not found");
+    }
+}
---
+++ MoneyTracker.Calculation/Bill/Frequencies/IFrequency.cs
@@ -0,0 +1,9 @@
+﻿
+using MoneyTracker.Shared.Models.Bill;
+
+namespace MoneyTracker.Calculation.Bill.Frequencies;
+internal interface IFrequency
+{
+    public bool MatchCommand(string frequency);
+    public OverDueBillInfo? Calculate(DateOnly nextDueDate);
+}
