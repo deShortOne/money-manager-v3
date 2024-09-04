16d1f46245d9fc4a4bf4a303c160db9f5f38229f
deShortOne <jingshianggu@gmail.com>
2024-09-05 00:23:54 +0100

    initial commit for bill test

---
+++ MoneyTracker.Tests/Unit/Calculation/Bill/OverDueBillInfo/Daily.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MoneyTracker.Tests.Unit.Calculation.Bill.OverDueBillInfo;
+public sealed class Daily
+{
+    [Fact]
+    public void NoOverDue()
+    {
+        Assert.True(true);
+    }
+}
