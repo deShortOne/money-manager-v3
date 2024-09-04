5b9c5f996bcd5dcc3c344baf64212fc9e81b5e2c
deShortOne <jingshianggu@gmail.com>
2024-09-04 08:02:18 +0100

    feat: add over due bill info

---
+++ MoneyTracker.Shared/Models/Bill/OverDueBillInfo.cs
@@ -0,0 +1,7 @@
+﻿
+namespace MoneyTracker.Shared.Models.Bill;
+public class OverDueBillInfo(int daysOverDue, int numberOfBillsOverDue)
+{
+    public int daysOverDue { get; private set; } = daysOverDue;
+    public int numberOfBillsOverDue { get; private set; } = numberOfBillsOverDue;
+}
