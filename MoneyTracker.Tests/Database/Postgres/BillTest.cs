6a3e1e33003de4109b166e9ee339f775187325e4
deShortOne <jingshianggu@gmail.com>
2024-09-04 12:58:18 +0100

    feat: initial bill test

---
+++ MoneyTracker.Tests/Database/Postgres/BillTest.cs
@@ -0,0 +1,44 @@
+﻿
+using MoneyTracker.Data.Postgres;
+using MoneyTracker.DatabaseMigration;
+using MoneyTracker.Shared.Models.Bill;
+using MoneyTracker.Tests.Database.Postgres.TestModels;
+using Newtonsoft.Json;
+using Testcontainers.PostgreSql;
+
+namespace MoneyTracker.Tests.Database.Postgres;
+public class BillTest : IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _postgres = new PostgreSqlBuilder()
+#if RUN_LOCAL
+        .WithDockerEndpoint("tcp://localhost:2375")
+#endif
+        .WithImage("postgres:16")
+        .WithCleanUp(true)
+        .Build();
+
+    public async Task InitializeAsync()
+    {
+        await _postgres.StartAsync();
+
+        Migration.CheckMigration(_postgres.GetConnectionString());
+
+        return;
+    }
+
+    public Task DisposeAsync()
+    {
+        return _postgres.DisposeAsync().AsTask();
+    }
+
+    [Fact]
+    public async void FirstLoadCheckTablesThatDataAreThere()
+    {
+        var db = new PostgresDatabase(_postgres.GetConnectionString());
+        var bill = new BillDatabase(db);
+
+        var actual = await bill.GetBill();
+
+        Assert.Empty(actual);
+    }
+}
