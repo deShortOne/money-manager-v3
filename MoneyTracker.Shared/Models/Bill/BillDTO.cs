28c32a28b9b4f65d1f3e13c3c7533c6cdd6e9722
deShortOne <jingshianggu@gmail.com>
2024-09-05 00:09:43 +0100

    feat: added equals

--- MoneyTracker.Shared/Models/Bill/BillDTO.cs
+++ MoneyTracker.Shared/Models/Bill/BillDTO.cs
@@ -8,4 +8,22 @@ public class BillDTO(int id, string payee, decimal amount, DateOnly nextDueDate,
     public DateOnly NextDueDate { get; private set; } = nextDueDate;
     public string Frequency { get; private set; } = frequency;
     public string Category { get; private set; } = category;
+
+    public override bool Equals(System.Object obj)
+    {
+        var other = obj as BillDTO;
+
+        if (other == null)
+        {
+            return false;
+        }
+
+        // Instances are considered equal if the ReferenceId matches.
+        return Payee == other.Payee && Amount == other.Amount && NextDueDate == other.NextDueDate && Frequency == other.Frequency && Category == other.Category;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id;
+    }
 }
