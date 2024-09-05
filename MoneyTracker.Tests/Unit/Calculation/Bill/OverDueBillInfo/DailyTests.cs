5df4630c3d3e00efd25bfeb8fa39903a5b1dca92
deShortOne <jingshianggu@gmail.com>
2024-09-05 01:07:17 +0100

    test: daily same day no days overdue

--- MoneyTracker.Tests/Unit/Calculation/Bill/OverDueBillInfo/DailyTests.cs
+++ MoneyTracker.Tests/Unit/Calculation/Bill/OverDueBillInfo/DailyTests.cs
@@ -1,10 +1,24 @@
 ﻿
+using Microsoft.Extensions.Time.Testing;
+using MoneyTracker.Calculation.Bill.Frequencies;
+using MoneyTracker.Shared.DateManager;
+using Moq;
+
 namespace MoneyTracker.Tests.Unit.Calculation.Bill.OverDueBillInfo;
 public sealed class DailyTests
 {
     [Fact]
     public void NoOverDue()
     {
-        Assert.True(true);
+        // Given
+        var mockDateTime = new Mock<IDateTimeProvider>();
+        mockDateTime.Setup(dateTime => dateTime.Now).Returns(new DateTime(2024, 8, 24, 0, 0, 0));
+
+        IDateTimeProvider dateTimeProvider = mockDateTime.Object;
+
+        var day = new Daily();
+
+        // Then
+        Assert.Null(day.Calculate(new DateOnly(2024, 8, 24), dateTimeProvider));
     }
 }
