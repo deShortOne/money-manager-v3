9a6909d8b0dd314d37f4351e0b09a487aa795408
deShortOne <jingshianggu@gmail.com>
2024-09-05 21:05:27 +0100

    test(weekly): ensures key matches

---
+++ MoneyTracker.Tests/Unit/Calculation/Bill/OverDueBill/WeeklyTests.cs
@@ -0,0 +1,12 @@
+﻿
+using MoneyTracker.Calculation.Bill.Frequencies;
+
+namespace MoneyTracker.Tests.Unit.Calculation.Bill.OverDueBill;
+public sealed class WeeklyTests
+{
+    [Fact]
+    public void CalculateOverDueBillInfo_NameMatch_True()
+    {
+        Assert.True(new Weekly().MatchCommand("Weekly"));
+    }
+}
