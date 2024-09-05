0eb2f33dbc7c99addb7de57aea8553e08eb06c0e
deShortOne <jingshianggu@gmail.com>
2024-09-05 21:45:30 +0100

    test(biweekly): key match

---
+++ MoneyTracker.Calculation/Bill/Frequencies/BiWeekly.cs
@@ -0,0 +1,10 @@
+﻿
+using MoneyTracker.Shared.DateManager;
+using MoneyTracker.Shared.Models.Bill;
+
+namespace MoneyTracker.Calculation.Bill.Frequencies;
+internal class BiWeekly : IFrequency
+{
+    public OverDueBillInfo? Calculate(DateOnly nextDueDate, IDateTimeProvider dateTimeProvider) => throw new NotImplementedException();
+    public bool MatchCommand(string frequency) => frequency == "BiWeekly";
+}
---
+++ MoneyTracker.Tests/Unit/Calculation/Bill/OverDueBill/BiWeeklyTests.cs
@@ -0,0 +1,12 @@
+﻿
+using MoneyTracker.Calculation.Bill.Frequencies;
+
+namespace MoneyTracker.Tests.Unit.Calculation.Bill.OverDueBill;
+public sealed class BiWeeklyTests
+{
+    [Fact]
+    public void CalculateOverDueBillInfo_NameMatch_True()
+    {
+        Assert.True(new BiWeekly().MatchCommand("BiWeekly"));
+    }
+}
