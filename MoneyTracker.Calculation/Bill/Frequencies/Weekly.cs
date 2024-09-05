f95d1e491fc87f47e377c1cab0e777a6a6fe275c
deShortOne <jingshianggu@gmail.com>
2024-09-05 21:14:01 +0100

    test(weekly): wihtin 1 week before nextduedate

--- MoneyTracker.Calculation/Bill/Frequencies/Weekly.cs
+++ MoneyTracker.Calculation/Bill/Frequencies/Weekly.cs
@@ -5,6 +5,17 @@ using MoneyTracker.Shared.Models.Bill;
 namespace MoneyTracker.Calculation.Bill.Frequencies;
 internal class Weekly : IFrequency
 {
-    public OverDueBillInfo? Calculate(DateOnly nextDueDate, IDateTimeProvider dateTimeProvider) => throw new NotImplementedException();
+    public OverDueBillInfo? Calculate(DateOnly nextDueDate, IDateTimeProvider dateTimeProvider)
+    {
+        var today = DateOnly.FromDateTime(dateTimeProvider.Now);
+        int numberOfDaysOverdue = today.DayNumber - nextDueDate.DayNumber;
+
+        if (numberOfDaysOverdue <= 0)
+        {
+            return null;
+        }
+        throw new NotImplementedException();
+    }
+
     public bool MatchCommand(string frequency) => frequency == "Weekly";
 }
--- MoneyTracker.Tests/Unit/Calculation/Bill/OverDueBill/WeeklyTests.cs
+++ MoneyTracker.Tests/Unit/Calculation/Bill/OverDueBill/WeeklyTests.cs
@@ -1,5 +1,7 @@
 ﻿
 using MoneyTracker.Calculation.Bill.Frequencies;
+using MoneyTracker.Shared.DateManager;
+using MoneyTracker.Tests.Local;
 
 namespace MoneyTracker.Tests.Unit.Calculation.Bill.OverDueBill;
 public sealed class WeeklyTests
@@ -8,5 +10,18 @@ public sealed class WeeklyTests
     public void CalculateOverDueBillInfo_NameMatch_True()
     {
         Assert.True(new Weekly().MatchCommand("Weekly"));
+    }
+
+    [Fact]
+    public void CalculateOverDueBillInfo_WithinOneWeekBefore_Null()
+    {
+        IDateTimeProvider currentDay = TestHelper.CreateMockDateTimeProvider(new DateTime(2024, 8, 24, 0, 0, 0));
+
+        var week = new Weekly();
+
+        Assert.Null(week.Calculate(new DateOnly(2024, 8, 24), currentDay));
+        Assert.Null(week.Calculate(new DateOnly(2024, 8, 26), currentDay));
+        Assert.Null(week.Calculate(new DateOnly(2024, 8, 28), currentDay));
+        Assert.Null(week.Calculate(new DateOnly(2024, 8, 30), currentDay));
     }
 }
