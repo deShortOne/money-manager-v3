0963de7c431548a07a8a227c1afc7c6bcb286d62
deShortOne <jingshianggu@gmail.com>
2024-09-06 18:19:42 +0100

    test(monthly): before nextduedate

--- MoneyTracker.Calculation/Bill/Frequencies/Monthly.cs
+++ MoneyTracker.Calculation/Bill/Frequencies/Monthly.cs
@@ -5,6 +5,16 @@ using MoneyTracker.Shared.Models.Bill;
 namespace MoneyTracker.Calculation.Bill.Frequencies;
 internal class Monthly : IFrequency
 {
-    public OverDueBillInfo? Calculate(DateOnly nextDueDate, IDateTimeProvider dateTimeProvider) => throw new NotImplementedException();
+    public OverDueBillInfo? Calculate(DateOnly nextDueDate, IDateTimeProvider dateTimeProvider)
+    {
+        var today = DateOnly.FromDateTime(dateTimeProvider.Now);
+        int numberOfDaysOverdue = today.DayNumber - nextDueDate.DayNumber;
+
+        if (numberOfDaysOverdue <= 0)
+        {
+            return null;
+        }
+        throw new NotImplementedException();
+    }
     public bool MatchCommand(string frequency) => frequency == "Monthly";
 }
--- MoneyTracker.Tests/Unit/Calculation/Bill/OverDueBill/MonthlyTests.cs
+++ MoneyTracker.Tests/Unit/Calculation/Bill/OverDueBill/MonthlyTests.cs
@@ -1,5 +1,7 @@
 ﻿
 using MoneyTracker.Calculation.Bill.Frequencies;
+using MoneyTracker.Shared.DateManager;
+using MoneyTracker.Tests.Local;
 
 namespace MoneyTracker.Tests.Unit.Calculation.Bill.OverDueBill;
 public sealed class MonthlyTests
@@ -8,5 +10,25 @@ public sealed class MonthlyTests
     public void CalculateOverDueBillInfo_NameMatch_True()
     {
         Assert.True(new Monthly().MatchCommand("Monthly"));
+    }
+
+    [Fact]
+    public void CalculateOverDueBillInfo_SameDay_Null()
+    {
+        IDateTimeProvider dateTimeProvider = TestHelper.CreateMockDateTimeProvider(new DateTime(2024, 8, 24, 0, 0, 0));
+
+        var month = new Monthly();
+
+        Assert.Null(month.Calculate(new DateOnly(2024, 8, 24), dateTimeProvider));
+    }
+
+    [Fact]
+    public void CalculateOverDueBillInfo_BeforeNextDueDate_Null()
+    {
+        IDateTimeProvider dateTimeProvider = TestHelper.CreateMockDateTimeProvider(new DateTime(2024, 8, 24, 0, 0, 0));
+
+        var month = new Monthly();
+
+        Assert.Null(month.Calculate(new DateOnly(2024, 8, 30), dateTimeProvider));
     }
 }
