c178833a48247d6be1cb8e67e60aba5c453a4fbe
deShortOne <jingshianggu@gmail.com>
2024-09-06 18:11:59 +0100

    test(monthly): key match

---
+++ MoneyTracker.Tests/Unit/Calculation/Bill/OverDueBill/MonthlyTests.cs
@@ -0,0 +1,12 @@
+﻿
+using MoneyTracker.Calculation.Bill.Frequencies;
+
+namespace MoneyTracker.Tests.Unit.Calculation.Bill.OverDueBill;
+public sealed class MonthlyTests
+{
+    [Fact]
+    public void CalculateOverDueBillInfo_NameMatch_True()
+    {
+        Assert.True(new Monthly().MatchCommand("Monthly"));
+    }
+}
