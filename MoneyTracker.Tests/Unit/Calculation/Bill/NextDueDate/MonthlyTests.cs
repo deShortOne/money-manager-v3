56ae8c5bae5658c01e80fb93678a64b616bf42f3
deShortOne <jingshianggu@gmail.com>
2024-09-07 09:56:14 +0100

    test(monthly): next due date 31st to 31st

--- MoneyTracker.Tests/Unit/Calculation/Bill/NextDueDate/MonthlyTests.cs
+++ MoneyTracker.Tests/Unit/Calculation/Bill/NextDueDate/MonthlyTests.cs
@@ -15,4 +15,15 @@ public sealed class MonthlyTests
         var nextDueDate = month.CalculateNextDueDate(24, new DateOnly(2024, 8, 24), timeProvider);
         Assert.Equal(new DateOnly(2024, 9, 24), nextDueDate);
     }
+
+    [Fact]
+    public void GetNextDueDate_31stTo31st()
+    {
+        var timeProvider = TestHelper.CreateMockDateTimeProvider(new DateTime(2024, 08, 24, 0, 0, 0));
+
+        var month = new Monthly();
+
+        var nextDueDate = month.CalculateNextDueDate(31, new DateOnly(2024, 7, 31), timeProvider);
+        Assert.Equal(new DateOnly(2024, 8, 31), nextDueDate);
+    }
 }
