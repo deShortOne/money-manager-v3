b1d2f088a3ddf087f52d9e75e55b813dc7568e76
deShortOne <jingshianggu@gmail.com>
2024-09-07 09:54:16 +0100

    test(monthly): get next due date from 24th

--- MoneyTracker.Calculation/Bill/Frequencies/Monthly.cs
+++ MoneyTracker.Calculation/Bill/Frequencies/Monthly.cs
@@ -5,7 +5,10 @@ using MoneyTracker.Shared.Models.Bill;
 namespace MoneyTracker.Calculation.Bill.Frequencies;
 internal class Monthly : IFrequency
 {
-    public DateOnly CalculateNextDueDate(int monthDay, DateOnly currNextDueDate, IDateTimeProvider dateTimeProvider) => throw new NotImplementedException();
+    public DateOnly CalculateNextDueDate(int monthDay, DateOnly currNextDueDate, IDateTimeProvider dateTimeProvider)
+    {
+        return currNextDueDate.AddMonths(1);
+    }
 
     public OverDueBillInfo? CalculateOverDueBill(DateOnly nextDueDate, IDateTimeProvider dateTimeProvider)
     {
---
+++ MoneyTracker.Tests/Unit/Calculation/Bill/NextDueDate/MonthlyTests.cs
@@ -0,0 +1,18 @@
+﻿
+using MoneyTracker.Calculation.Bill.Frequencies;
+using MoneyTracker.Tests.Local;
+
+namespace MoneyTracker.Tests.Unit.Calculation.Bill.NextDueDate;
+public sealed class MonthlyTests
+{
+    [Fact]
+    public void GetNextDueDate()
+    {
+        var timeProvider = TestHelper.CreateMockDateTimeProvider(new DateTime(2024, 08, 24, 0, 0, 0));
+
+        var month = new Monthly();
+
+        var nextDueDate = month.CalculateNextDueDate(24, new DateOnly(2024, 8, 24), timeProvider);
+        Assert.Equal(new DateOnly(2024, 9, 24), nextDueDate);
+    }
+}
