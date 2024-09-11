ff10bab03b6426ffce337eb1087e65ae5c4dd4a1
deShortOne <jingshianggu@gmail.com>
2024-09-11 22:16:30 +0100

    test(bill service): repliacted from bill db but using service

--- MoneyTracker.Tests/Core/BillServiceTest.cs
+++ MoneyTracker.Tests/Core/BillServiceTest.cs
@@ -31,14 +31,12 @@ public class BillServiceTest : IAsyncLifetime
     {
         return _postgres.DisposeAsync().AsTask();
     }
-
+    [Fact]
     public async void FirstLoadCheckTablesThatDataAreThere()
     {
         var db = new PostgresDatabase(_postgres.GetConnectionString());
         IDateProvider dateProvider = TestHelper.CreateMockdateProvider(new DateOnly(2024, 8, 24));
         var bill = new BillDatabase(db, dateProvider);
-
-        var billService = new BillService(bill, dateProvider);
 
         var expected = new List<BillDTO>()
         {
@@ -47,6 +45,134 @@ public class BillServiceTest : IAsyncLifetime
         };
 
         var actual = await bill.GetAllBills();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async void DeleteBill()
+    {
+        var db = new PostgresDatabase(_postgres.GetConnectionString());
+        IDateProvider dateProvider = TestHelper.CreateMockdateProvider(new DateOnly(2024, 8, 24));
+        var bill = new BillDatabase(db, dateProvider);
+
+        var billService = new BillService(bill, dateProvider);
+        await billService.DeleteBill(new DeleteBillDTO(1));
+
+        var expected = new List<BillDTO>()
+        {
+            new BillDTO(2, "company a", 100, DateOnly.Parse("2024-08-30"), "Monthly", "Wages & Salary : Net Pay", null),
+        };
+
+        var actual = await billService.GetAllBills();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async void EditBill()
+    {
+        var db = new PostgresDatabase(_postgres.GetConnectionString());
+        IDateProvider dateProvider = TestHelper.CreateMockdateProvider(new DateOnly(2024, 8, 24));
+        var bill = new BillDatabase(db, dateProvider);
+
+        var billService = new BillService(bill, dateProvider);
+        await billService.EditBill(new EditBillDTO(1, payee: "supermarket b"));
+
+        var expected = new List<BillDTO>()
+        {
+            new BillDTO(2, "company a", 100, DateOnly.Parse("2024-08-30"), "Monthly", "Wages & Salary : Net Pay", null),
+            new BillDTO(1, "supermarket b", 23, DateOnly.Parse("2024-09-03"), "Weekly", "Groceries", null),
+        };
+
+        var actual = await billService.GetAllBills();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async void AddBill()
+    {
+        var db = new PostgresDatabase(_postgres.GetConnectionString());
+        IDateProvider dateProvider = TestHelper.CreateMockdateProvider(new DateOnly(2024, 8, 24));
+        var bill = new BillDatabase(db, dateProvider);
+
+        var billService = new BillService(bill, dateProvider);
+        await billService.AddBill(new NewBillDTO("flight sim", 420, DateOnly.Parse("2024-09-05"), "Daily", 5));
+
+        var expected = new List<BillDTO>()
+        {
+            new BillDTO(2, "company a", 100, DateOnly.Parse("2024-08-30"), "Monthly", "Wages & Salary : Net Pay", null),
+            new BillDTO(1, "supermarket a", 23, DateOnly.Parse("2024-09-03"), "Weekly", "Groceries", null),
+            new BillDTO(3, "flight sim", 420, DateOnly.Parse("2024-09-05"), "Daily", "Hobby", null),
+        };
+
+        var actual = await billService.GetAllBills();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async void FirstLoadCheckButCurrentDateIsOneIterationAfterDueDate()
+    {
+        var db = new PostgresDatabase(_postgres.GetConnectionString());
+        IDateProvider dateProvider = TestHelper.CreateMockdateProvider(new DateOnly(2024, 9, 7));
+        var bill = new BillDatabase(db, dateProvider);
+
+        var billService = new BillService(bill, dateProvider);
+        var expected = new List<BillDTO>()
+        {
+            new BillDTO(2, "company a", 100, DateOnly.Parse("2024-08-30"), "Monthly", "Wages & Salary : Net Pay",
+                new OverDueBillInfo(8, 1)),
+            new BillDTO(1, "supermarket a", 23, DateOnly.Parse("2024-09-03"), "Weekly", "Groceries",
+                new OverDueBillInfo(4, 1)),
+        };
+
+        var actual = await billService.GetAllBills();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async void FirstLoadCheckButCurrentDateIsMultipleIterationsAfterDueDate()
+    {
+        var db = new PostgresDatabase(_postgres.GetConnectionString());
+        IDateProvider dateProvider = TestHelper.CreateMockdateProvider(new DateOnly(2024, 10, 4));
+        var bill = new BillDatabase(db, dateProvider);
+
+        var billService = new BillService(bill, dateProvider);
+
+        var expected = new List<BillDTO>()
+        {
+            new BillDTO(2, "company a", 100, DateOnly.Parse("2024-08-30"), "Monthly", "Wages & Salary : Net Pay",
+                new OverDueBillInfo(35, 2)),
+            new BillDTO(1, "supermarket a", 23, DateOnly.Parse("2024-09-03"), "Weekly", "Groceries",
+                new OverDueBillInfo(31, 5)),
+        };
+
+        var actual = await billService.GetAllBills();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async void FirstLoadCheckButCurrentDateIsMultipleIterationsAfterDueDate2()
+    {
+        var db = new PostgresDatabase(_postgres.GetConnectionString());
+        IDateProvider dateProvider = TestHelper.CreateMockdateProvider(new DateOnly(2024, 10, 3));
+        var bill = new BillDatabase(db, dateProvider);
+
+        var billService = new BillService(bill, dateProvider);
+
+        var expected = new List<BillDTO>()
+        {
+            new BillDTO(2, "company a", 100, DateOnly.Parse("2024-08-30"), "Monthly", "Wages & Salary : Net Pay",
+                new OverDueBillInfo(34, 2)),
+            new BillDTO(1, "supermarket a", 23, DateOnly.Parse("2024-09-03"), "Weekly", "Groceries",
+                new OverDueBillInfo(30, 5)),
+        };
+
+        var actual = await billService.GetAllBills();
 
         Assert.Equal(expected, actual);
     }
