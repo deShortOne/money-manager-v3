c953e9f6c4c52625ccd2c09f5d04b87fe7eac9d4
deShortOne <jingshianggu@gmail.com>
2024-09-13 22:52:58 +0100

    test(bill): refactor database out into init

--- MoneyTracker.Tests/Core/BillServiceTest.cs
+++ MoneyTracker.Tests/Core/BillServiceTest.cs
@@ -18,11 +18,16 @@ public class BillServiceTest : IAsyncLifetime
         .WithCleanUp(true)
         .Build();
 
+    private BillDatabase _billDb;
+
     public async Task InitializeAsync()
     {
         await _postgres.StartAsync();
 
         Migration.CheckMigration(_postgres.GetConnectionString(), new MigrationOption(true));
+
+        var db = new PostgresDatabase(_postgres.GetConnectionString());
+        _billDb = new BillDatabase(db);
 
         return;
     }
@@ -34,17 +39,13 @@ public class BillServiceTest : IAsyncLifetime
     [Fact]
     public async void FirstLoadCheckTablesThatDataAreThere()
     {
-        var db = new PostgresDatabase(_postgres.GetConnectionString());
-        IDateProvider dateProvider = TestHelper.CreateMockdateProvider(new DateOnly(2024, 8, 24));
-        var bill = new BillDatabase(db);
-
         var expected = new List<BillDTO>()
         {
             new BillDTO(2, "company a", 100, DateOnly.Parse("2024-08-30"), "Monthly", "Wages & Salary : Net Pay", null),
             new BillDTO(1, "supermarket a", 23, DateOnly.Parse("2024-09-03"), "Weekly", "Groceries", null),
         };
 
-        var actual = await bill.GetAllBills();
+        var actual = await _billDb.GetAllBills();
 
         Assert.Equal(expected, actual);
     }
@@ -52,11 +53,9 @@ public class BillServiceTest : IAsyncLifetime
     [Fact]
     public async void DeleteBill()
     {
-        var db = new PostgresDatabase(_postgres.GetConnectionString());
         IDateProvider dateProvider = TestHelper.CreateMockdateProvider(new DateOnly(2024, 8, 24));
-        var bill = new BillDatabase(db);
+        var billService = new BillService(_billDb, dateProvider);
 
-        var billService = new BillService(bill, dateProvider);
         await billService.DeleteBill(new DeleteBillDTO(1));
 
         var expected = new List<BillDTO>()
@@ -72,11 +71,9 @@ public class BillServiceTest : IAsyncLifetime
     [Fact]
     public async void EditBill()
     {
-        var db = new PostgresDatabase(_postgres.GetConnectionString());
-        var bill = new BillDatabase(db);
-
         IDateProvider dateProvider = TestHelper.CreateMockdateProvider(new DateOnly(2024, 8, 24));
-        var billService = new BillService(bill, dateProvider);
+        var billService = new BillService(_billDb, dateProvider);
+
         await billService.EditBill(new EditBillDTO(1, payee: "supermarket b"));
 
         var expected = new List<BillDTO>()
@@ -93,11 +90,9 @@ public class BillServiceTest : IAsyncLifetime
     [Fact]
     public async void AddBill()
     {
-        var db = new PostgresDatabase(_postgres.GetConnectionString());
-        var bill = new BillDatabase(db);
-
         IDateProvider dateProvider = TestHelper.CreateMockdateProvider(new DateOnly(2024, 8, 24));
-        var billService = new BillService(bill, dateProvider);
+        var billService = new BillService(_billDb, dateProvider);
+
         await billService.AddBill(new NewBillDTO("flight sim", 420, DateOnly.Parse("2024-09-05"), "Daily", 5));
 
         var expected = new List<BillDTO>()
@@ -115,11 +110,8 @@ public class BillServiceTest : IAsyncLifetime
     [Fact]
     public async void FirstLoadCheckButCurrentDateIsOneIterationAfterDueDate()
     {
-        var db = new PostgresDatabase(_postgres.GetConnectionString());
-        var bill = new BillDatabase(db);
-
         IDateProvider dateProvider = TestHelper.CreateMockdateProvider(new DateOnly(2024, 9, 7));
-        var billService = new BillService(bill, dateProvider);
+        var billService = new BillService(_billDb, dateProvider);
 
         var expected = new List<BillDTO>()
         {
@@ -137,11 +129,8 @@ public class BillServiceTest : IAsyncLifetime
     [Fact]
     public async void FirstLoadCheckButCurrentDateIsMultipleIterationsAfterDueDate()
     {
-        var db = new PostgresDatabase(_postgres.GetConnectionString());
-        var bill = new BillDatabase(db);
-
         IDateProvider dateProvider = TestHelper.CreateMockdateProvider(new DateOnly(2024, 10, 4));
-        var billService = new BillService(bill, dateProvider);
+        var billService = new BillService(_billDb, dateProvider);
 
         DateOnly[] dates = [new DateOnly(2024, 9, 3), new DateOnly(2024, 9, 10), new DateOnly(2024, 9, 17),
             new DateOnly(2024, 9, 24), new DateOnly(2024, 10, 1)];
@@ -161,11 +150,8 @@ public class BillServiceTest : IAsyncLifetime
     [Fact]
     public async void FirstLoadCheckButCurrentDateIsMultipleIterationsAfterDueDate2()
     {
-        var db = new PostgresDatabase(_postgres.GetConnectionString());
-        var bill = new BillDatabase(db);
-
         IDateProvider dateProvider = TestHelper.CreateMockdateProvider(new DateOnly(2024, 10, 3));
-        var billService = new BillService(bill, dateProvider);
+        var billService = new BillService(_billDb, dateProvider);
 
         DateOnly[] dates = [new DateOnly(2024, 9, 3), new DateOnly(2024, 9, 10), new DateOnly(2024, 9, 17),
             new DateOnly(2024, 9, 24), new DateOnly(2024, 10, 1)];
@@ -185,11 +171,8 @@ public class BillServiceTest : IAsyncLifetime
     [Fact]
     public async void SkipOccurence_SkipTwoOccurences_BillIsSkippedPassed()
     {
-        var db = new PostgresDatabase(_postgres.GetConnectionString());
-        var bill = new BillDatabase(db);
-
         IDateProvider dateProvider = TestHelper.CreateMockdateProvider(new DateOnly(2024, 10, 3));
-        var billService = new BillService(bill, dateProvider);
+        var billService = new BillService(_billDb, dateProvider);
 
         var currNewBill = await billService.SkipOccurence(new SkipBillOccurrenceDTO(1, new DateOnly(2024, 9, 17)));
 
