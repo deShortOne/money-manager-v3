669517690216ed138daddc416801dd476b4acb53
deShortOne <jingshianggu@gmail.com>
2024-09-16 19:58:35 +0100

    chore: jwt config

---
+++ MoneyTracker.Shared/Auth/JwtConfig.cs
@@ -0,0 +1,20 @@
+﻿
+namespace MoneyTracker.Shared.Auth;
+public interface IJwtConfig
+{
+    string Audience { get; }
+    int Expires { get; }
+    string Issuer { get; }
+    string Key { get; }
+}
+
+public class JwtConfig(string issuer, string audience, string key, int expires) : IJwtConfig
+{
+    public string Issuer => issuer;
+
+    public string Audience => audience;
+
+    public string Key => key;
+
+    public int Expires => expires;
+}
