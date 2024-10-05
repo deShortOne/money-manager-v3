1d850b7c9f7a80c2f6381bbb8bb8b8a0fbd82ea4
deShortOne <jingshianggu@gmail.com>
2024-10-05 18:47:15 +0100

    start moving authentication test over

--- MoneyTracker.Shared/Auth/LoginWithUsernameAndPassword.cs
+++ MoneyTracker.Shared/Auth/LoginWithUsernameAndPassword.cs
@@ -2,4 +2,16 @@
 public class LoginWithUsernameAndPassword(string username)
 {
     public string Username { get; } = username;
+
+    public override bool Equals(object? obj)
+    {
+        var other = obj as LoginWithUsernameAndPassword;
+        if (other == null) return false;
+        return Username == other.Username;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Username);
+    }
 }
---
+++ Tests/MoneyTracker.Authentication/Service/AuthenticateUserTest.cs
@@ -0,0 +1,57 @@
+﻿using MoneyTracker.Core;
+using MoneyTracker.Shared.Auth;
+using MoneyTracker.Shared.Data;
+using MoneyTracker.Shared.DateManager;
+using Moq;
+
+namespace MoneyTracker.Authentication.Service;
+
+public class AuthenticateUserTest
+{
+    [Fact]
+    public async void SuccessfullyLogInUser1()
+    {
+        var userToAuthenticate = new LoginWithUsernameAndPassword("root");
+        var expected = new AuthenticatedUser(1);
+
+        var userDb = new Mock<IUserAuthDatabase>();
+        userDb.Setup(x => x.AuthenticateUser(It.Is<LoginWithUsernameAndPassword>(y => y == userToAuthenticate)))
+            .Returns(Task.FromResult(expected));
+
+        var jwtToken = new JwtConfig("", "", "", 0);
+        var userAuthService = new UserAuthenticationService(userDb.Object, jwtToken, new DateTimeProvider());
+
+        Assert.Equal(expected, await userAuthService.AuthenticateUser(userToAuthenticate));
+    }
+
+    [Fact]
+    public async void SuccessfullyLogInUser2()
+    {
+        var userToAuthenticate = new LoginWithUsernameAndPassword("secondary root");
+        var expected = new AuthenticatedUser(2);
+
+        var userDb = new Mock<IUserAuthDatabase>();
+        userDb.Setup(x => x.AuthenticateUser(It.Is<LoginWithUsernameAndPassword>(y => y == userToAuthenticate)))
+            .Returns(Task.FromResult(expected));
+
+        var jwtToken = new JwtConfig("", "", "", 0);
+        var userAuthService = new UserAuthenticationService(userDb.Object, jwtToken, new DateTimeProvider());
+
+        Assert.Equal(expected, await userAuthService.AuthenticateUser(userToAuthenticate));
+    }
+
+    [Fact]
+    public async void FailToLogInUserThatDoesntExist()
+    {
+        //var userToAuthenticate = new LoginWithUsernameAndPassword("broken root");
+
+        //var userDb = new Mock<IUserAuthDatabase>();
+        //var jwtToken = new JwtConfig("", "", "", 0);
+        //var userAuthService = new UserAuthenticationService(userDb.Object, jwtToken, new DateTimeProvider());
+
+        //await Assert.ThrowsAsync<InvalidDataException>(async () =>
+        //{
+        //    await userAuthService.AuthenticateUser(userToAuthenticate);
+        //});
+    }
+}
