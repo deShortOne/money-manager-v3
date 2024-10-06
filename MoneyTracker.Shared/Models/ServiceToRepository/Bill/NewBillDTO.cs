5a58a0e52ef454b29cb2c86d737f5b66480eb1d6
deShortOne <jingshianggu@gmail.com>
2024-10-06 20:52:02 +0100

    how adding a new bill is currently done

--- MoneyTracker.Shared/Models/ServiceToRepository/Bill/NewBillDTO.cs
+++ MoneyTracker.Shared/Models/ServiceToRepository/Bill/NewBillDTO.cs
@@ -10,4 +10,22 @@ public class NewBillDTO(string payee, decimal amount, DateOnly nextDueDate, stri
     public int Category { get; private set; } = category;
     public int MonthDay { get; private set; } = monthDay;
     public int AccountId { get; } = accountId;
+
+    public override bool Equals(object? obj)
+    {
+        var other = obj as NewBillDTO;
+        if (other == null) return false;
+        return Payee == other.Payee &&
+            Amount == other.Amount &&
+            NextDueDate == other.NextDueDate &&
+            Frequency == other.Frequency &&
+            Category == other.Category &&
+            MonthDay == other.MonthDay &&
+            AccountId == other.AccountId;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Payee, Payee, Amount, NextDueDate, Frequency, Category, MonthDay, AccountId);
+    }
 }
---
+++ Tests/MoneyTracker.Bill.Tests/Service/AddBillTest.cs
@@ -0,0 +1,57 @@
+﻿
+using MoneyTracker.Core;
+using MoneyTracker.Data.Postgres;
+using MoneyTracker.Shared.Auth;
+using MoneyTracker.Shared.Core;
+using MoneyTracker.Shared.Data;
+using MoneyTracker.Shared.DateManager;
+using MoneyTracker.Shared.Models.ControllerToService.Bill;
+using MoneyTracker.Shared.Models.ServiceToRepository.Bill;
+using Moq;
+
+namespace MoneyTracker.Bill.Tests.Service;
+public sealed class AddBillTest
+{
+    [Fact]
+    public async void SuccessfullyAddNewBill()
+    {
+        var userId = 52;
+        var authedUser = new AuthenticatedUser(userId);
+        var tokenToDecode = "tokenToDecode";
+
+        var payee = "bree";
+        var amount = 75.24m;
+        var nextDueDate = new DateOnly(2024, 1, 24);
+        var frequency = "Weekly";
+        var category = 1;
+        var monthDay = 24;
+        var accountId = 2;
+        var newBillRequest = new NewBillRequestDTO(payee, amount, nextDueDate, frequency, category, monthDay, accountId);
+        var newBillEntity = new NewBillDTO(payee, amount, nextDueDate, frequency, category, monthDay, accountId);
+
+        var mockDateProvider = new Mock<IDateProvider>();
+
+        var mockUserAuthService = new Mock<IUserAuthenticationService>();
+        mockUserAuthService.Setup(x => x.DecodeToken(tokenToDecode)).Returns(Task.FromResult(authedUser));
+
+        var mockAccountDatabase = new Mock<IAccountDatabase>();
+        mockAccountDatabase.Setup(x => x.IsAccountOwnedByUser(authedUser, accountId)).Returns(Task.FromResult(true));
+
+        var mockBillDatabase = new Mock<IBillDatabase>();
+        mockBillDatabase.Setup(x => x.AddBill(newBillEntity));
+
+        var billService = new BillService(mockBillDatabase.Object,
+            mockDateProvider.Object,
+            mockUserAuthService.Object,
+            mockAccountDatabase.Object);
+
+        await billService.AddBill(tokenToDecode, newBillRequest);
+
+        Assert.Multiple(() =>
+        {
+            mockUserAuthService.Verify(x => x.DecodeToken(tokenToDecode), Times.Once);
+            mockAccountDatabase.Verify(x => x.IsAccountOwnedByUser(authedUser, accountId), Times.Once);
+            mockBillDatabase.Verify(x => x.AddBill(newBillEntity), Times.Once);
+        });
+    }
+}
