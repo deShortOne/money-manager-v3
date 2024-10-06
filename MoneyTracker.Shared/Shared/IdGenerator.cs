ff6f722ba408f08d007f16e60a11b14794099389
deShortOne <jingshianggu@gmail.com>
2024-10-06 21:00:54 +0100

    id generator can generate new int

--- MoneyTracker.Shared/Shared/IdGenerator.cs
+++ MoneyTracker.Shared/Shared/IdGenerator.cs
@@ -4,9 +4,11 @@ namespace MoneyTracker.Shared.Shared;
 public interface IIdGenerator
 {
     Guid NewGuid { get; }
+    int NewInt(int prevId);
 }
 
 public class IdGenerator : IIdGenerator
 {
     public Guid NewGuid => Guid.NewGuid();
+    public int NewInt(int prevId) => prevId + 1;
 }
