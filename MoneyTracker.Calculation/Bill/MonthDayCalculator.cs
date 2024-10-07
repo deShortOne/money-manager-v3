b5b6b625856175a41919b86e6e9c18be79a30cc4
deShortOne <jingshianggu@gmail.com>
2024-10-07 19:32:43 +0100

    feat: calculate month day

---
+++ MoneyTracker.Calculation/Bill/MonthDayCalculator.cs
@@ -0,0 +1,14 @@
+﻿
+namespace MoneyTracker.Calculation.Bill;
+public class MonthDayCalculator
+{
+    public int Calculate(DateOnly date)
+    {
+        var nextDate = date.AddDays(1);
+        if (date.Month != nextDate.Month)
+        {
+            return 31;
+        }
+        return date.Day;
+    }
+}
