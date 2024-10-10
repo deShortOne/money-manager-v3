9e29e67976e503425081e01ac948b2624e0d5ab1
deShortOne <jingshianggu@gmail.com>
2024-10-10 23:02:07 +0100

    moved database construction into helper

--- Tests/MoneyTracker.Bill.Tests/Repository/GetLastIdTest.cs
+++ Tests/MoneyTracker.Bill.Tests/Repository/GetLastIdTest.cs
@@ -1,37 +1,10 @@
 ﻿
-using MoneyTracker.Data.Postgres;
 using MoneyTracker.DatabaseMigration;
 using MoneyTracker.DatabaseMigration.Models;
-using MoneyTracker.Shared.Data;
-using Testcontainers.PostgreSql;
 
 namespace MoneyTracker.Bill.Tests.Repository;
-public sealed class GetLastIdTest : IAsyncLifetime
+public sealed class GetLastIdTest : BillRespositoryTestHelper
 {
-    private readonly PostgreSqlContainer _postgres = new PostgreSqlBuilder()
-#if RUN_LOCAL
-        .WithDockerEndpoint("tcp://localhost:2375")
-#endif
-        .WithImage("postgres:16")
-        .WithCleanUp(true)
-        .Build();
-
-    private IBillDatabase _billRepo;
-
-    public async Task InitializeAsync()
-    {
-        await _postgres.StartAsync();
-        Migration.CheckMigration(_postgres.GetConnectionString(), new MigrationOption());
-
-        var _database = new PostgresDatabase(_postgres.GetConnectionString());
-        _billRepo = new BillDatabase(_database);
-    }
-
-    public Task DisposeAsync()
-    {
-        return _postgres.DisposeAsync().AsTask();
-    }
-
     [Fact]
     public async void GetLastIdWithNoDataInTables()
     {
