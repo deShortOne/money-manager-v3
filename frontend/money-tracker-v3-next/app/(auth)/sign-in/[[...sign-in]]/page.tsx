9f4e1c98d2742fd3e43863dc492fc671f6b03d52
deShortOne <jingshianggu@gmail.com>
2024-12-19 21:42:31 +0000

    created sign up and login pages

---
+++ frontend/money-tracker-v3-next/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,92 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+export default function A() {
+
+    const formSchema = z.object({
+        // TODO: add validation
+        username: z.string(),
+        password: z.string(),
+    })
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            password: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+    return (
+        <div className="inline-flex items-center justify-center w-full h-[90vh]">
+            <Card className="w-1/4">
+                <CardHeader>
+                    <CardTitle>Welcome back!</CardTitle>
+                    <CardDescription>Please log in</CardDescription>
+                </CardHeader>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="">
+                        <CardContent>
+                            <FormField
+                                control={form.control}
+                                name="username"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <Input placeholder="Username" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <div className="mt-2" />
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <Input placeholder="Password" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </CardContent>
+                        <CardFooter className="flex justify-between">
+                            <Button type="submit">Sign in</Button>
+                        </CardFooter>
+                    </form>
+                </Form>
+            </Card>
+        </div >
+    )
+}
