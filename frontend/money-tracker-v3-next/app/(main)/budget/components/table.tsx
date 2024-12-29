c4b21a23d5ad60e89ee32146e0e6d1e27c3c5c4e
deShortOne <jingshianggu@gmail.com>
2024-12-29 22:45:19 +0000

    shows budget category under budget group
    it is possible to hide budget categories if budget group is clicked on
    shows by default as it's not immediately obvious that categories can be hidden/ revealed

--- frontend/money-tracker-v3-next/app/(main)/budget/components/table.tsx
+++ frontend/money-tracker-v3-next/app/(main)/budget/components/table.tsx
@@ -12,24 +12,33 @@ import { getAllBudgets } from "./action";
 import { useCookies } from "react-cookie";
 import { useQuery } from "@tanstack/react-query";
 import { Result } from "@/types/result";
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 export default function BudgetsDisplay() {
+    const [expandedBudgetCategory, setExpandedBudgetCategory] = useState<string[]>([])
+    const toggleRow = (index: string) => {
+        if (expandedBudgetCategory.includes(index)) {
+            setExpandedBudgetCategory(expandedBudgetCategory.filter((i) => i !== index))
+        } else {
+            setExpandedBudgetCategory([...expandedBudgetCategory, index])
+        }
+    }
+
     const [cookies] = useCookies(['token']);
-
     const [transactions, setTransactions] = useState<BudgetGroup[]>([]);
-
     const { status, data, error, isFetching } = useQuery<Result<BudgetGroup[]>>({
         queryKey: ['transactions'],
         queryFn: () => {
             return getAllBudgets(cookies.token)
         },
     });
-
     useEffect(() => {
         if (data == null || data.hasError || data.item == undefined) {
 
         } else {
-            setTransactions(data?.item)
+            setExpandedBudgetCategory(data.item.map(i => i.name))
+            setTransactions(data.item)
         }
     }, [data]);
 
@@ -45,12 +54,33 @@ export default function BudgetsDisplay() {
             </TableHeader>
             <TableBody>
                 {transactions.map((transaction) => (
-                    <TableRow key={transaction.name}>
-                        <TableCell>{transaction.name}</TableCell>
-                        <TableCell className="text-right">{transaction.actual}</TableCell>
-                        <TableCell className="text-right">{transaction.planned}</TableCell>
-                        <TableCell className="text-right">{transaction.difference}</TableCell>
-                    </TableRow>
+                    <React.Fragment key={transaction.name}>
+                        <TableRow>
+                            <TableCell>
+                                <Button variant="ghost" onClick={() => toggleRow(transaction.name)}>
+                                    {transaction.name}
+                                </Button>
+                            </TableCell>
+                            <TableCell className="text-right">{transaction.actual}</TableCell>
+                            <TableCell className="text-right">{transaction.planned}</TableCell>
+                            <TableCell className="text-right">{transaction.difference}</TableCell>
+                        </TableRow>
+
+                        {expandedBudgetCategory.includes(transaction.name) && (
+                            <>
+                                {transaction.categories.map((category) => (
+                                    <TableRow key={category.name}>
+                                        <TableCell className="">
+                                            <span className="ml-8">{category.name}</span>
+                                        </TableCell>
+                                        <TableCell className="text-right">{category.actual}</TableCell>
+                                        <TableCell className="text-right">{category.planned}</TableCell>
+                                        <TableCell className="text-right">{category.difference}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </>
+                        )}
+                    </React.Fragment>
                 ))}
             </TableBody>
         </Table>
