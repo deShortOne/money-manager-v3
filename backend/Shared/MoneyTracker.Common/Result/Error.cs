8f0f9c861a92fd0789b16b4fe60bbe8b502e3bf8
deShortOne <jingshianggu@gmail.com>
2025-01-11 18:05:29 +0000

    implemented result pattern but not added anywhere
    going to add to entire project
    essentially copied from https://github.com/karanraj-tech/result-pattern
    
    error types
    failure represents an unexpected error, should probably contain
    information related to the stack trace etc. not 100% how will work but
    that's the idea -> this should be like the catch all
    
    not found represents missing data or user attempting to access data that
    does not exist
    
    validation represents either domain objects being in an invalid state or
    user tries to input values that are incorrect, i.e. £45..00
    
    access unauthorised where user attempts to access data when they don't
    have access to it, i.e. an invalid token

---
+++ backend/Shared/MoneyTracker.Common/Result/Error.cs
@@ -0,0 +1,30 @@
+﻿
+namespace MoneyTracker.Common.Result;
+public class Error
+{
+    private Error(
+        string code,
+        string description,
+        ErrorType errorType
+        )
+    {
+        Code = code;
+        Description = description;
+        ErrorType = errorType;
+    }
+    public string Code { get; }
+    public string Description { get; }
+    public ErrorType ErrorType { get; }
+
+    public static Error Failure(string code, string description) =>
+        new(code, description, ErrorType.Failure);
+
+    public static Error NotFound(string code, string description) =>
+        new(code, description, ErrorType.NotFound);
+
+    public static Error Validation(string code, string description) =>
+        new(code, description, ErrorType.Validation);
+
+    public static Error AccessUnAuthorised(string code, string description) =>
+        new(code, description, ErrorType.AccessUnAuthorised);
+}
