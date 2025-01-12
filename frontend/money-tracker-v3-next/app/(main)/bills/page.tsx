e2ad42c779600aab620f8830dc1ef989b177d5b1
deShortOne <jingshianggu@gmail.com>
2025-01-12 22:25:07 +0000

    moved QueryClientProvider to topmost layer
    so that it doesn't need to be redeclared a billion and one times
    but also so it can reach pages that aren't so easy to implement the
    provider

--- frontend/money-tracker-v3-next/app/(main)/bills/page.tsx
+++ frontend/money-tracker-v3-next/app/(main)/bills/page.tsx
@@ -5,25 +5,18 @@ import SideBar from "./components/side-bar";
 import BillsDisplay from "./components/table";
 import { useCookies } from 'react-cookie';
 
-import {
-    QueryClient,
-    QueryClientProvider,
-} from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
-
 export default function Register() {
     const [cookies] = useCookies(['token']);
     if (cookies == null) {
 
     }
     return (
-        <QueryClientProvider client={queryClient}>
+        <>
             <UpdateBillForm />
             <div className="flex">
                 <SideBar />
                 <BillsDisplay />
             </div>
-        </QueryClientProvider>
+        </>
     )
 }
--- frontend/money-tracker-v3-next/app/(main)/register/page.tsx
+++ frontend/money-tracker-v3-next/app/(main)/register/page.tsx
@@ -2,23 +2,17 @@
 
 import TransactionsDisplay from "./components/table";
 
-import {
-    QueryClient,
-    QueryClientProvider,
-} from "@tanstack/react-query";
 import { UpdateTransactionForm } from "./components/update-transaction-form";
 import SideBar from "./components/side-bar";
 
-const queryClient = new QueryClient();
-
 export default function Register() {
     return (
-        <QueryClientProvider client={queryClient}>
+        <>
             <UpdateTransactionForm />
             <div className="flex">
                 <SideBar />
                 <TransactionsDisplay />
             </div>
-        </QueryClientProvider>
+        </>
     )
 }
