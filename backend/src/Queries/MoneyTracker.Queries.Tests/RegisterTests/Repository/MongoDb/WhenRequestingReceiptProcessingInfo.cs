a63bb974ec8a5e43ba9d910e92f770b7c6da28a4
deShortOne <jingshianggu@gmail.com>
2025-07-06 18:55:04 +0100

    feat: test use testcontainer of mongo instead of mocking

--- backend/src/Queries/MoneyTracker.Queries.Tests/RegisterTests/Repository/MongoDb/WhenRequestingReceiptProcessingInfo.cs
+++ backend/src/Queries/MoneyTracker.Queries.Tests/RegisterTests/Repository/MongoDb/WhenRequestingReceiptProcessingInfo.cs
@@ -1,18 +1,18 @@
 
 using MoneyTracker.Queries.Infrastructure.Mongo;
-using Moq;
+using MoneyTracker.Queries.Tests.Fixture;
 
 namespace MoneyTracker.Queries.Tests.RegisterTests.Repository.MongoDb;
-public class WhenRequestingReceiptProcessingInfo
+public class WhenRequestingReceiptProcessingInfo : IClassFixture<MongoDbFixture>
 {
     private RegisterCache _registerCache;
 
-    public WhenRequestingReceiptProcessingInfo()
+    public WhenRequestingReceiptProcessingInfo(MongoDbFixture mongoDbFixture)
     {
-        _registerCache = new RegisterCache(Mock.Of<MongoDatabase>());
+        _registerCache = new RegisterCache(new MongoDatabase(mongoDbFixture.ConnectionString));
     }
 
-    //[Fact]
+    [Fact]
     public async Task ThenAnExceptionIsThrown()
     {
         var message = await Assert.ThrowsAsync<NotImplementedException>(async () => await _registerCache.GetReceiptProcessingInfo("", CancellationToken.None));
